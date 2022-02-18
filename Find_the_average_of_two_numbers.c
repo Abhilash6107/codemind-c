04a2d305f21824381cfdf18880047de95b6f65f4
Abhilash6107 <97166690+Abhilash6107@users.noreply.github.com>
2022-02-18 22:22:57 +0530

    #include<stdio.h> int main() {     float N,M,average;     scanf("%f%f",&N,&M);     if(1<=N,M<=1000)     {         average=(N+M)/2;         printf("%0.4f",average);     }     return 0; }

---
+++ Find_the_average_of_two_numbers.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    float N,M,average;
+    scanf("%f%f",&N,&M);
+    if(1<=N,M<=1000)
+    {
+        average=(N+M)/2;
+        printf("%0.4f",average);
+    }
+    return 0;
+}
